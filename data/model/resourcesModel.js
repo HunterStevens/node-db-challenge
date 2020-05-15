074f9385bd5eea4348a57c7ea4e5c9cba29dfc2f
Hunter Stevens <hunterstevens1996@gmail.com>
2020-05-15 10:13:51 -0600

    made skeleton model functions for each table

---
+++ data/model/resourcesModel.js
@@ -0,0 +1,24 @@
+const db = require('../dbConfig');
+
+module.exports ={
+
+}
+
+function findAll(){
+
+}
+
+function findById(id){
+
+}
+
+function add(){
+
+}
+
+function remove(id){
+
+}
+function update(id){
+    
+}
