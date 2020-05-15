b88ee46669fcc352b870d37db0a89c32d90d31c5
Hunter Stevens <hunterstevens1996@gmail.com>
2020-05-15 09:55:49 -0600

    made the migrate file with all of the tables and created some empty Model files

--- data/migrations/20200515084254_projects_tables.js
+++ data/migrations/20200515084254_projects_tables.js
@@ -1,6 +1,35 @@
 
 exports.up = function(knex) {
-  
+  return knex.schema.createTable('projects', proj =>{
+        proj.increments();
+        proj.string('name').unique().notNullable();
+        proj.string('description');
+        proj.boolean('complete').notNullable().defaultTo(false);
+
+  })
+  .createTable('resources', res =>{
+      res.increments();
+      res.string('name').unique().notNullable();
+      res.string('description');
+})
+.createTable('tasks', task =>{
+      task.increments();
+      task.string('description').unique().notNullable();
+      task.boolean('complete').notNullable().defaultTo(false);
+      task.string('notes');
+      task.integer('project_id').unsigned().notNullable()
+      .references('projects.id')
+      .onUpdate('cascade').onDelete('CASCADE');
+})
+.createTable('projects_resources', projRes =>{
+      projRes.increments();
+      projRes.integer('project_id').unsigned().notNullable()
+      .references('projects.id')
+      .onUpdate('cascade').onDelete('CASCADE');
+      projRes.integer('resource_id').unsigned().notNullable()
+      .references('resources.id')
+      .onUpdate('cascade').onDelete('CASCADE');
+})
 };
 
 exports.down = function(knex) {
